874b8b8660efdb7142e81116c48ccc1098266c78
Cedrick <cedrick@nabupro.com>
2024-10-29 20:58:38 -0400

    test

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ export default {
    * @return {Promise<Response>}
    */
   async fetch(request, env, executionContext) {
+    console.log('test');
     try {
       const appLoadContext = await createAppLoadContext(
         request,
@@ -57,7 +58,6 @@ export default {
 
       return response;
     } catch (error) {
-      // eslint-disable-next-line no-console
       console.error(error);
       return new Response('An unexpected error occurred', {status: 500});
     }
